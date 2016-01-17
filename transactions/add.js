69b9b93d3dc2663d9bc41b79866b0ca7668ab465
Ross Mackay <ross@mcky.me>
2016-01-17 16:37:44 +0000

    Throttle adding transactions to sheet

--- transactions/add.js
+++ transactions/add.js
@@ -1,11 +1,17 @@
 import sheets from '../sheets.js'
 import formatTransactions from './format.js'
 
+const sleep = (duration) => {
+	const now = new Date().getTime()
+	while(new Date().getTime() < now + duration){}
+}
+
 const addTransactionsToSheet = (transactions) => {
 	sheets.auth()
 		.then(sheet => {
 			transactions.forEach(transaction => {
 				sheets.addRow(sheet, transaction)
+				sleep(200)
 			})
 		})
 		.catch(err => {
